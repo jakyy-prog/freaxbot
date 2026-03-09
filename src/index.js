2893f46b7492d2f476b9e28e353f0997cf280714
vzaki <146656230+jakyy-prog@users.noreply.github.com>
2026-03-09 15:49:13 +0700

    Mencoba deploy dengan railway ke 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ require("dotenv").config();
 
 const client = new REdoClient();
 client.login(process.env.DISCORD_BOT_TOKEN);
-
-client.once("ready", async () => {
-  await client.application.commands.set([]);
-  console.log("🔥 GLOBAL COMMANDS CLEARED");
-});
